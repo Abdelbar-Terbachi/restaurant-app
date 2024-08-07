2866efbc2c5b7641e079b93c968f6b3316ae6409
Abdelbar Terbachi <65946077+Abdelbar-Terbachi@users.noreply.github.com>
2024-08-07 11:19:57 +0100

    fix snackbar and form clearing after submission

--- client/src/components/reservation/ReservationForm.tsx
+++ client/src/components/reservation/ReservationForm.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { SnackbarProvider, useSnackbar } from "notistack";
-import React from "react";
-import { Box, Grid, TextField, Button } from "@mui/material";
-import { useFormik, FormikProps } from "formik";
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { FormikProps, useFormik } from "formik";
+import { useSnackbar } from "notistack";
 import * as yup from "yup";
 
 import { usePathname } from "next/navigation";
@@ -27,7 +26,7 @@ const validationSchema = yup.object({
 
 const ReservationForm = () => {
   const pathname = usePathname();
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
 
   const formik: FormikProps<ReservationFormValues> =
     useFormik<ReservationFormValues>({
@@ -40,112 +39,111 @@ const ReservationForm = () => {
         message: "",
       },
       validationSchema: validationSchema,
-      onSubmit: (values) => {
+      onSubmit: (values, { resetForm }) => {
         enqueueSnackbar("Email was sent successfully", { variant: "success" });
+        resetForm();
       },
     });
 
   return (
-    <SnackbarProvider>
-      <Box
-        component="form"
-        onSubmit={formik.handleSubmit}
-        sx={{ flexGrow: 1, padding: 2, mt: "5rem", width: "100%" }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6} mb="2rem">
-            <TextField
-              fullWidth
-              id="fullName"
-              name="fullName"
-              label="Full Name"
-              value={formik.values.fullName}
-              onChange={formik.handleChange}
-              error={formik.touched.fullName && Boolean(formik.errors.fullName)}
-              helperText={formik.touched.fullName && formik.errors.fullName}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} mb="2rem">
-            <TextField
-              fullWidth
-              id="date"
-              name="date"
-              label="Date"
-              type="date"
-              InputLabelProps={{ shrink: true }}
-              value={formik.values.date}
-              onChange={formik.handleChange}
-              error={formik.touched.date && Boolean(formik.errors.date)}
-              helperText={formik.touched.date && formik.errors.date}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} mb="2rem">
-            <TextField
-              fullWidth
-              id="hour"
-              name="hour"
-              label="Hour"
-              type="time"
-              InputLabelProps={{ shrink: true }}
-              value={formik.values.hour}
-              onChange={formik.handleChange}
-              error={formik.touched.hour && Boolean(formik.errors.hour)}
-              helperText={formik.touched.hour && formik.errors.hour}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} mb="2rem">
-            <TextField
-              fullWidth
-              id="email"
-              name="email"
-              label="Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} mb="2rem">
-            <TextField
-              fullWidth
-              id="phone"
-              name="phone"
-              label="Phone Number"
-              type="number"
-              value={formik.values.phone}
-              onChange={formik.handleChange}
-              error={formik.touched.phone && Boolean(formik.errors.phone)}
-              helperText={formik.touched.phone && formik.errors.phone}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} mb="2rem">
-            <TextField
-              fullWidth
-              id="message"
-              name="message"
-              label="Message"
-              multiline
-              rows={4}
-              value={formik.values.message}
-              onChange={formik.handleChange}
-              error={formik.touched.message && Boolean(formik.errors.message)}
-              helperText={formik.touched.message && formik.errors.message}
-            />
-          </Grid>
-          <Grid item xs={12} mb="2rem">
-            <Button
-              color="primary"
-              variant="contained"
-              fullWidth
-              type="submit"
-              sx={{ mt: "1rem" }}
-            >
-              {pathname === "/contact" ? "Send" : "Book"}
-            </Button>
-          </Grid>
+    <Box
+      component="form"
+      onSubmit={formik.handleSubmit}
+      sx={{ flexGrow: 1, padding: 2, mt: "5rem", width: "100%" }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6} mb="2rem">
+          <TextField
+            fullWidth
+            id="fullName"
+            name="fullName"
+            label="Full Name"
+            value={formik.values.fullName}
+            onChange={formik.handleChange}
+            error={formik.touched.fullName && Boolean(formik.errors.fullName)}
+            helperText={formik.touched.fullName && formik.errors.fullName}
+          />
         </Grid>
-      </Box>
-    </SnackbarProvider>
+        <Grid item xs={12} md={6} mb="2rem">
+          <TextField
+            fullWidth
+            id="date"
+            name="date"
+            label="Date"
+            type="date"
+            InputLabelProps={{ shrink: true }}
+            value={formik.values.date}
+            onChange={formik.handleChange}
+            error={formik.touched.date && Boolean(formik.errors.date)}
+            helperText={formik.touched.date && formik.errors.date}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} mb="2rem">
+          <TextField
+            fullWidth
+            id="hour"
+            name="hour"
+            label="Hour"
+            type="time"
+            InputLabelProps={{ shrink: true }}
+            value={formik.values.hour}
+            onChange={formik.handleChange}
+            error={formik.touched.hour && Boolean(formik.errors.hour)}
+            helperText={formik.touched.hour && formik.errors.hour}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} mb="2rem">
+          <TextField
+            fullWidth
+            id="email"
+            name="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} mb="2rem">
+          <TextField
+            fullWidth
+            id="phone"
+            name="phone"
+            label="Phone Number"
+            type="number"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            error={formik.touched.phone && Boolean(formik.errors.phone)}
+            helperText={formik.touched.phone && formik.errors.phone}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} mb="2rem">
+          <TextField
+            fullWidth
+            id="message"
+            name="message"
+            label="Message"
+            multiline
+            rows={4}
+            value={formik.values.message}
+            onChange={formik.handleChange}
+            error={formik.touched.message && Boolean(formik.errors.message)}
+            helperText={formik.touched.message && formik.errors.message}
+          />
+        </Grid>
+        <Grid item xs={12} mb="2rem">
+          <Button
+            color="primary"
+            variant="contained"
+            fullWidth
+            type="submit"
+            sx={{ mt: "1rem" }}
+          >
+            {pathname === "/contact" ? "Send" : "Book"}
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
